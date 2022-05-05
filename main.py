39de6b3297aae7ebb8379abc12522678e09640fd
Pabilito <pborsukiewicz@onet.pl>
2022-05-05 21:33:19 +0200

    Activation functions

--- main.py
+++ main.py
@@ -32,3 +32,19 @@ metrics = np.array(dataset)
 
 #Divide dataset into training and testing set, we can change random_state to change the shuffling of the data
 train_metrics, test_metrics, train_toPredict, test_toPredict = train_test_split(metrics, toPredict, test_size = 0.75, random_state = 42)
+
+#Activation functions
+def activation_sigmoid(x):
+    return 1/(1 + np.exp(-x))
+def activation_tanh(x):
+    return np.tanh(x)
+
+#Variables used during training
+inputLayerNeurons = 2                                              # Input layer neurons
+hiddenLayersNeurons = [3,3]                                        # Hidden layer neurons
+outputLayerNeurons = 2                                             # Output layer neurons
+learningRate = 0.01                                                # Learning rate
+epochs = 100                                                       # Epochs
+iasHiddenValue = np.random.uniform(-1, 1)                          # Bias for hidden Layer
+BiasOutputValue = np.random.uniform(-1, 1)                         # Bias for output Layer
+
